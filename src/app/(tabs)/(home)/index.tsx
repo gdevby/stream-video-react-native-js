3d544ae44259c44c44bc1b6d387da05715344c09
Nikita Korevo <47-korevo@users.noreply.gitlab.gdev.by>
2025-04-24 15:06:19 +0300

    feat: create BroadcastsButton component

--- src/app/(tabs)/(home)/index.tsx
+++ src/app/(tabs)/(home)/index.tsx
@@ -1,37 +1,14 @@
-import { VideoPlayerIcon } from '@/src/components/icons/VideoPlayerIcon';
 import { ScreenLayout } from '@/src/components/ScreenLayout';
-import { Color } from '@/src/lib/constants/Color';
 import { Banner } from '@/src/modules/home/components/Banner';
-import { StyleSheet, Text, View } from 'react-native';
+import { BroadcastsButton } from '@/src/modules/home/components/BroadcastsButton';
 
 const HomeScreen = () => {
   return (
     <ScreenLayout styles={{ display: 'flex', gap: 8 }}>
       <Banner />
-      <View style={styles.videoPlayerContainer}>
-        <Text style={styles.text}>Трансляции</Text>
-        <VideoPlayerIcon />
-      </View>
+      <BroadcastsButton />
     </ScreenLayout>
   );
 };
-
-const styles = StyleSheet.create({
-  videoPlayerContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 12,
-    paddingVertical: 17,
-    backgroundColor: Color.LightBlue,
-    borderRadius: 16,
-  },
-
-  text: {
-    fontSize: 18,
-    color: '#0C3955',
-    fontWeight: 600,
-  },
-});
 
 export default HomeScreen;
---
+++ src/modules/home/components/BroadcastsButton/BroadcastsButton.tsx
@@ -0,0 +1,30 @@
+import { VideoPlayerIcon } from '@/src/components/icons/VideoPlayerIcon';
+import { Color } from '@/src/lib/constants/Color';
+import { StyleSheet, Text, View } from 'react-native';
+
+export const BroadcastsButton = () => {
+  return (
+    <View style={styles.videoPlayerContainer}>
+      <Text style={styles.text}>Трансляции</Text>
+      <VideoPlayerIcon />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  videoPlayerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+    paddingVertical: 17,
+    backgroundColor: Color.LightBlue,
+    borderRadius: 16,
+  },
+
+  text: {
+    fontSize: 18,
+    color: '#0C3955',
+    fontWeight: 600,
+  },
+});
