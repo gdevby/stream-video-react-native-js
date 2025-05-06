684b99d4c05efcc96056b3d61825597842fb299b
Nikita Korevo <47-korevo@users.noreply.gitlab.gdev.by>
2025-05-06 18:21:58 +0300

    feat: fix error "viewmanagerresolver returned null for either RNSScreenContentWrapper"

--- src/lib/constants/common.ts
+++ src/lib/constants/common.ts
@@ -1,4 +1,4 @@
 export const calendarUrl =
   'https://docs.google.com/spreadsheets/u/2/d/e/2PACX-1vQJVbia4YD2CkMshwHiVHVABIba7YkMPa4owSCr2HfmpC6dQI4xNxBWbLnwn-iR_CfwSEMlSYnGfieS/pubhtml#';
-export const BASE_BACKEND_PATH = 'https://back.art-caramel.ru';
-export const BASE_BACKEND_PATH_API = 'https://back.art-caramel.ru/back/api';
+export const BASE_BACKEND_PATH = 'https://prod-back.art-caramel.ru';
+export const BASE_BACKEND_PATH_API = 'https://prod-back.art-caramel.ru/back/api';
